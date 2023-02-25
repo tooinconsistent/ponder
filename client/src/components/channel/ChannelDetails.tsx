6e2825b96fc4a7f5f99922d0cc181999f0e04626
Andreea Nastase <andreanastase@gmail.com>
2023-02-25 22:59:03 +0100

    private channel

--- client/src/components/channel/ChannelDetails.tsx
+++ client/src/components/channel/ChannelDetails.tsx
@@ -1,18 +1,23 @@
 import { Component } from "solid-js";
+import { Private } from "./icons/Private.jsx";
 
 import { buttonClasses } from "@tooinconsistent/client/components/atoms/button.js";
 
 interface ChannelDetailsProps {
   name: string;
   description: string;
+  isPrivate?: boolean;
 }
 
 export const ChannelDetails: Component<ChannelDetailsProps> = (props) => {
   return (
     <div class="px-11 py-8  flex justify-between items-end">
       <div>
-        <div class="text-3xl font-bold text-[var(--textHeader-foreground)]">
-          {props.name}
+        <div class="flex flex-row space-x-2 items-center">
+          <div class="text-3xl font-bold text-[var(--textHeader-foreground)]">
+            {props.name}
+          </div>
+          {props.isPrivate && <Private />}
         </div>
         <div class="leading-none mt-1">{props.description}</div>
       </div>
